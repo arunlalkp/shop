8836bbc87722259a8da64a5cc983746e20dc3d7b
Arun Lal <arunlalkp.kp@gmail.com>
2019-03-04 10:16:08 +0530

    installed mongoose and connected to mongodb, completed #3 Mongodb/mongoose setup in tutorial

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressHbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost:27017/shop',{ useNewUrlParser: true });
 // view engine setup
 app.engine('.hbs',expressHbs({defaultLayout: 'layout', extname:'.hbs'}));
 app.set('view engine', '.hbs');
