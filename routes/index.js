9e89bb90b4b9ce18de0782c08c74484431ff751d
Arun Lal <arunlalkp.kp@gmail.com>
2019-03-04 10:02:48 +0530

    first content view added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hellooooo' });
+  res.render('shop/index', { title: 'Hellooooo' });
 });
 
 module.exports = router;
