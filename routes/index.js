91d8747e144f9483d230a59bffe5c11df1a9b72e
Arun Lal <arunlalkp.kp@gmail.com>
2019-03-04 12:18:25 +0530

    completed #5 outputing Data, Output the data in shop db to products viwe

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const Product = require('../models/product')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Hellooooo' });
+
+  Product.find(function (err, docs) {
+    let productChunks = [];
+    let chunkSize = 4;
+    for (let i = 0; i < docs.length; i += chunkSize) {
+      productChunks.push(docs.slice(i, i + chunkSize));
+    }
+    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+  })
 });
 
 module.exports = router;
