ac7e24fd175636dd2c1da64481b73135a6a6ebd5
Arun Lal <arunlalkp.kp@gmail.com>
2019-03-06 09:54:15 +0530

    added some ui changes, signin and singn up pages are aligned center, password help tag is addded in sign up form

--- routes/index.js
+++ routes/index.js
@@ -6,31 +6,30 @@ const Cart = require('../models/cart');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  Product.find(function (err, docs) {
-    let productChunks = [];
-    let chunkSize = 4;
-    for (let i = 0; i < docs.length; i += chunkSize) {
-      productChunks.push(docs.slice(i, i + chunkSize));
-    }
-    res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
-  })
+    Product.find(function (err, docs) {
+        let productChunks = [];
+        let chunkSize = 4;
+        for (let i = 0; i < docs.length; i += chunkSize) {
+            productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+    })
 });
 
 router.get('/add-to-cart/:id', function (req, res, next) {
-  const productId = req.params.id;
-  const cart = new Cart(req.session.cart ? req.session.cart : {});
+    const productId = req.params.id;
+    const cart = new Cart(req.session.cart ? req.session.cart : {});
 
-  Product.findById(productId, function (err, product) {
-    if (err) {
-      res.redirect('/');
-    }
-    cart.add(product, product.id);
-    req.session.cart = cart;
-    console.log( req.session.cart);
-    res.redirect('/');
-  })
+    Product.findById(productId, function (err, product) {
+        if (err) {
+            res.redirect('/');
+        }
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        console.log( req.session.cart);
+        res.redirect('/');
+    })
 });
-
 
 
 module.exports = router;
