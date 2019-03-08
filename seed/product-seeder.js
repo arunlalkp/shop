26dde0a3dc248cbf4828fbb33cb22caa5f6fb8ca
Arun Lal <arunlalkp.kp@gmail.com>
2019-03-08 15:22:51 +0530

    product information changed

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -6,39 +6,45 @@ mongoose.connect('mongodb://localhost:27017/shop',{ useNewUrlParser: true });
 
 const products = [
     new Product({
-        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/CAT-W73239_89703-05y.psd?wid=790&hei=657&fmt=jpeg&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72',
-        title: 'Caterpiller',
-        description: 'The Best shoes in the world, That is CAT Footwear!',
-        price: 160
+        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/P720683_New_Angle?wid=186&hei=160&fmt=jpg&bgc=ffffff&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72&align=0,1',
+        title: 'PREZ WATERPROOF SHOE',
+        description: 'Waterproof leather upper and Antimicrobial microfiber lining ',
+        price: 70
     }),
     new Product({
-        imagePath: 'https://s7d2.scene7.com/is/image/academy/10648086?wid=410&hei=410',
-        title: 'CAT',
-        description: 'The Best shoes in the world, That is CAT Footwear!',
+        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/CATM-P723054-110717-F18-000?wid=186&hei=160&fmt=jpg&bgc=ffffff&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72&align=0,1',
+        title: 'DATA SHOE',
+        description: 'Cement Construction for added durability ',
+        price: 70
+    }),
+    new Product({
+        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/CATM-P722227-011618-F18-000?wid=186&hei=160&fmt=jpg&bgc=ffffff&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72&align=0,1',
+        title: 'TRANSFORM 2.0 BOOT',
+        description: 'Waterproof leather upper and Antimicrobial microfiber lining ',
+        price: 90
+    }),
+    new Product({
+        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/P717819_1?wid=186&hei=160&fmt=jpg&bgc=ffffff&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72&align=0,1',
+        title: 'FOUNDER BOOT',
+        description: ' Nylon Mesh Lining and Nylex TM Sock Liner',
+        price: 115
+    }),
+    new Product({
+        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/CATM-P721593-092016-F17-000?wid=186&hei=160&fmt=jpg&bgc=ffffff&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72&align=0,1',
+        title: 'FOUNDER WATERPROOF BOOT',
+        description: 'Waterproof nubuck upper lining and Soft, breathable nylon mesh lining',
         price: 120
     }),
     new Product({
-        imagePath: 'https://www.catworkwear.com.au/media/catalog/product/c/a/catm-p720055-1.jpg',
-        title: 'CATerpiller',
-        description: 'The Best shoes in the world, That is CAT Footwear!',
-        price: 100
+        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/CATM-P721722-092016-F17-000?wid=186&hei=160&fmt=jpg&bgc=ffffff&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72&align=0,1',
+        title: 'DEPLETE WATERPROOF BOOT',
+        description: 'Waterproof leather upper and Antimicrobial microfiber lining ',
+        price: 110
     }),
     new Product({
-        imagePath: 'https://www.symbios.pk/image/cache/data/c/cat%20shoes%20heavy%20duty%20symbios%20pk4_1453657397-500x500.JPG',
-        title: 'Caterpiller',
-        description: 'The Best shoes in the world, That is CAT Footwear!',
-        price: 125
-    }),
-    new Product({
-        imagePath: 'https://ae01.alicdn.com/kf/HTB1YqPJfwLD8KJjSszeq6yGRpXao/CAT-Footwear-Men-Classic-Genuine-Cow-Leather-Working-Casual-Safety-Shoes-For-Male-Ankle-Winter-Boots.jpg_640x640.jpg',
-        title: 'Caterpiller',
-        description: 'The Best shoes in the world, That is CAT Footwear!',
-        price: 130
-    }),
-    new Product({
-        imagePath: 'https://www.symbios.pk/image/cache/data/c/cat%20shoes%20heavy%20duty%20symbios%20pk4_1453657397-500x500.JPG',
-        title: 'Caterpiller',
-        description: 'The Best shoes in the world, That is CAT Footwear!',
+        imagePath: 'https://a248.e.akamai.net/f/248/9086/10h/wolverine-o.scene7.com/is/image/WolverineWorldWide/CATM-P721724-092016-F17-000?wid=186&hei=160&fmt=jpg&bgc=ffffff&qlt=80&op_sharpen=0&resMode=sharp&op_usm=0.5,1.0,1.0&iccEmbed=0&printRes=72&align=0,1',
+        title: 'DEPLETE WATERPROOF BOOT DARK',
+        description: 'Durable and lightweight EVA midsole and Antimicrobial microfiber lining ',
         price: 110
     })
 ];
